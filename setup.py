4a5283005a5e564a1ef982524d1dcfca1e997bf7
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-03-14 08:49:58 +0100

    remove some demo code

--- setup.py
+++ setup.py
@@ -13,19 +13,3 @@ setup(
         "caldav == 0.1.4",
     ],
 )
-
-setup(
-    name='TowelStuff',
-    version='0.1.0',
-    author='J. Random Hacker',
-    author_email='jrh@example.com',
-    packages=['towelstuff', 'towelstuff.test'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
-    license='LICENSE.txt',
-    description='Useful towel-related stuff.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "selenium>=2.39.0"
-    ],
-)
